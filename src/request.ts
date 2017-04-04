3c83feab60beaf2abf4df94c3c18c607a7dd7cd0
Aitor Guerrero <aguerrerolasarte@gmail.com>
2017-04-04 13:38:19 +0200

    Fixed limit applied

--- src/request.ts
+++ src/request.ts
@@ -2,6 +2,7 @@ import {Readable} from 'stream';
 import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
 
 export interface Input {
+    Limit?: number;
     ExclusiveStartKey?: DocumentClient.AttributeMap;
 }
 
@@ -16,6 +17,7 @@ export class Request<I, O> extends Readable {
     private listCompleted: boolean;
     private cache: DocumentClient.AttributeMap[];
     private cachePos: number;
+    private left: number;
 
     constructor(
         private makeQuery: (i: I, callback: (err: Error, o: O & Output) => void) => void,
@@ -26,6 +28,7 @@ export class Request<I, O> extends Readable {
         this.cache = [];
         this.cachePos = 0;
         this.listCompleted = false;
+        this.left = input.Limit || Infinity;
     }
 
     async _read() {
@@ -47,14 +50,17 @@ export class Request<I, O> extends Readable {
     }
 
     private async loadBatch() {
+    	if (this.listCompleted) return;
         const result = await (new Promise<O & Output>((rs, rj) => {
             this.makeQuery(this.input, (err, result) => {
                 if (err) rj(err);
                 else rs(result);
             });
         }));
-        this.listCompleted = result.LastEvaluatedKey === undefined;
+        this.left -= result.Items.length;
         this.input.ExclusiveStartKey = result.LastEvaluatedKey;
+		this.input.Limit = this.left !== Infinity ? this.left : undefined;
+        this.listCompleted = this.input.ExclusiveStartKey === undefined || this.left <= 0;
         this.cache = result.Items;
     }
 }
