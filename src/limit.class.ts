eac20d79c906f8fc62fdf7217b45e2b4bdf28f69
Aitor Guerrero <aguerrerolasarte@gmail.com>
2017-10-18 17:37:37 +0200

    limit fixed

--- src/limit.class.ts
+++ src/limit.class.ts
@@ -11,11 +11,13 @@ export default class LimitStream extends Transform {
 	}
 
 	public _transform(item: any, enc: any, cb: any) {
+		if (this.count < this.limit) {
+			this.push(item);
+			this.count++;
+		}
 		if (this.count >= this.limit) {
 			this.emit(limitReachedEventName);
-		} else {
-			this.count++;
-			this.push(item);
+			this.destroy();
 		}
 		return cb();
 	}
